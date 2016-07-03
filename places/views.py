10ce74568b827d83afc3542c3112a63864be5360
Rustem Sayargaliev <r.sayargaliev@gmail.com>
2016-07-03 12:25:46 +0200

    random ordering, smaller interface

--- places/views.py
+++ places/views.py
@@ -5,6 +5,7 @@ from .models import Place
 
 class PlaceListView(ListView):
     model = Place
+    ordering = '?'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
